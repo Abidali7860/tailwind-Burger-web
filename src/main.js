1a47a19d72d113db2134c1da0cfe0a01e93c296f
aabid ali <aabid880036@gmail.com>
2024-09-12 21:07:20 +0530

    Burger-web

---
+++ src/main.js
@@ -0,0 +1,67 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+const closeIcon = document.getElementById('nav-close');
+const navLink = document.querySelectorAll('.nav__link');
+
+navLink.forEach(link =>
+    link.addEventListener('click', () => {
+        navMenu.classList.add('hidden')
+    }) 
+)
+
+closeIcon.addEventListener('click', () => {
+    navMenu.classList.add('hidden')
+})
+
+hamburger.addEventListener('click', () =>{
+navMenu.classList.remove('hidden')
+});
+
+// Tabs
+const tabs = document.querySelectorAll('.tabs_wrap ul li');
+const all = document.querySelectorAll('.item_wrap')
+const foods = document.querySelectorAll('.food');
+const snacks = document.querySelectorAll('.snack');
+const beverages = document.querySelectorAll('.beverage');
+tabs.forEach(tab => {
+    tab.addEventListener('click', () =>{
+        tabs.forEach(tab => {
+            tab.classList.remove("active")
+        })
+        tab.classList.add("active")
+        const tabVal = tab.getAttribute('data-tabs')
+        all.forEach(item =>{
+            item.style.display = "none" 
+        })
+        if (tabVal == 'food'){
+            foods.forEach(item =>{
+                item.style.display = "block" 
+            })
+        }
+       else if (tabVal == 'snack'){
+            snacks.forEach(item =>{
+                item.style.display = "block" 
+            })
+        }
+        else if (tabVal == 'beverage'){
+            beverages.forEach(item =>{
+                item.style.display = "block" 
+            })
+        }
+        else{
+            all.forEach(item =>{
+                item.style.display = "block" 
+        })}
+console.log(tabVal)
+    })
+})
+
+// Background change
+
+const body = document.querySelector('body')
+const Background = document.querySelector('#backgroundChange')
+Background.addEventListener('click', () =>{
+    body.classList.toggle('bg-black')
+    Background.classList.toggle('ri-sun-line')
+})
+
